66229d5b0e52a500e065385db5bc11940a9da775
diegbelg <conedie@gmail.com>
2020-09-15 02:58:47 +0000

    Sustitución de comandos

---
+++ 5_sustitucion_comandos.sh
@@ -0,0 +1,14 @@
+# ! /bin/bash
+# programa para revisar como ejecutar comandos dentro de un progtrama y almacenar en una variable para su posterior utilización de operadores
+# Author: Diego Beltran tw @diguitodev ista @diegodevelops
+
+
+ubicacionActual=`pwd`
+infoKernel=$(uname -a)
+echo "La ubicación actual es la siguiente: $ubicacionActual"
+echo "Información del Kernen: $infoKernel"
+
+
+
+
+
